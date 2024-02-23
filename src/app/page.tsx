0ab43244f4b2f348443e4c361c4be6d9f60838c7
MMMJB <michaelbeck445@gmail.com>
2024-02-22 23:58:01 -0500

    shut up vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,11 +49,11 @@ export default function Home() {
   return (
     <div className="flex w-full max-w-[600px] flex-col gap-8 p-8 sm:gap-24 sm:p-0">
       <div className="flex w-full gap-24">
-        <div className="flex flex-col gap-3">
-          <h1 className="font-semibold">Hey, I'm Michael! 👋</h1>
+        <div className="flex flex-col gap-1">
+          <h1 className="font-semibold">Hey, I&apos;m Michael! 👋</h1>
           <p className="text-justify text-sm">
             I’m a 16-year-old experienced in fullstack engineering and UI/UX
-            design. I’m the founder of{" "}
+            design. I&apos;m the founder of{" "}
             <a href="https://launchsite.tech">Launch</a> and a frontend
             developer at a <a href="/projects">few other projects</a>. If you
             have any questions, feel free to <a href="/contact">contact me</a>.
