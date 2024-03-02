7f6bdf5ce7790a8ed6093110096afd13b538e4da
MMMJB <michaelbeck445@gmail.com>
2024-03-01 23:01:54 -0500

    better layout & modified animations

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,24 +1,35 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 const navItems = [
   {
     title: "Skills",
     description: "Tools & technologies.",
     link: "/skills",
     icon: "/skills.png",
-    className: "group-hover:animate-pie",
+    animation: "animate-pie",
   },
   {
     title: "Projects",
     description: "My proudest works.",
     link: "/projects",
     icon: "/projects.png",
-    className: "group-hover:animate-spin-slow",
+    animation: "animate-spin-slow",
   },
   {
     title: "Journey",
     description: "How I got here.",
     link: "/journey",
     icon: "/journey.png",
-    className: "group-hover:animate-jump",
+    animation: "animate-jump",
+  },
+  {
+    title: "Contact",
+    description: "Let's get in touch!",
+    link: "/contact",
+    icon: "/contact.png",
+    animation: "animate-jiggle",
   },
 ];
 
@@ -27,21 +38,29 @@ export function NavigationCard({
   description,
   link,
   icon,
-  className = "",
+  animation,
 }: {
   title: string;
   description: string;
   link: string;
   icon: string;
-  className?: string;
+  animation: string;
 }) {
+  const [animationPlaying, setAnimationPlaying] = useState(false);
+
   return (
     <a
       href={link}
-      className="group flex w-full flex-col gap-12 rounded-md border border-card-light bg-card-light/5 p-3 transition-colors hover:bg-card-light/10 dark:border-card-dark/15 dark:bg-card-dark/5 dark:hover:bg-card-dark/10"
+      onMouseEnter={() => setAnimationPlaying(true)}
+      className="flex w-full flex-col gap-12 rounded-md border border-card-light bg-card-light/5 p-3 transition-colors hover:bg-card-light/10 dark:border-card-dark/15 dark:bg-card-dark/5 dark:hover:bg-card-dark/10"
     >
       <div className="h-8 w-8">
-        <img className={`h-full w-full ${className}`} alt="" src={icon} />
+        <img
+          onAnimationEnd={() => setAnimationPlaying(false)}
+          className={`h-full w-full ${animationPlaying && animation}`}
+          alt=""
+          src={icon}
+        />
       </div>
       <div className="flex flex-col text-end">
         <h3 className="font-semibold">{title}</h3>
@@ -54,18 +73,11 @@ export function NavigationCard({
 export default function Nav() {
   return (
     <div className="flex flex-col gap-3">
-      <nav className="flex w-full flex-col gap-3 sm:flex-row">
+      <nav className="grid w-full grid-cols-2 gap-3 sm:flex">
         {navItems.map((item) => (
           <NavigationCard key={item.title} {...item} />
         ))}
       </nav>
-      <NavigationCard
-        title="Contact"
-        description="Let's get in touch!"
-        link="/contact"
-        icon="/contact.png"
-        className="group-hover:animate-jiggle"
-      />
     </div>
   );
 }
